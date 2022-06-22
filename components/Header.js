66b095b36bb96dda4d68214e619015667511b972
Labib90 <labib724@gmail.com>
2022-06-22 20:04:48 +0700

    Challenge Header

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+// Kalian bisa menambahkan CSS di src/components/Header.css
+import React from 'react';
+
+const Header = () => {
+    return (
+    <div>
+    <h1 className="header">Call a Friend</h1>
+    <p className="tagline">your friendly contact app</p>
+    </div>
+    )
+}
+
+export default Header;
